b5a4f512cc8ff8ca0c2068a46393e1f4a1a896f9
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2016-04-07 16:25:42 -0600

    started route

--- routes/authors.js
+++ routes/authors.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('knex')(require('../knexfile')['development']);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('authors');
+  knex('authors')
+  .then(function(authors){
+    console.log(authors);
+    res.render('authors', {authors: authors});
+  })
 });
 
 module.exports = router;
