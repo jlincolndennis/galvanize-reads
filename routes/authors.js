fc68a1b4efc742d5b46a127adc9c468092112fc0
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2016-04-08 02:01:35 -0600

    AUTHORS VIEW IS WORKING!! I AM A GOLDEN GOD!!

--- routes/authors.js
+++ routes/authors.js
@@ -2,40 +2,53 @@ var express = require('express');
 var router = express.Router();
 var knex = require('knex')(require('../knexfile')['development']);
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-var fuckMyLife = {};
-  knex('authors')
-  .innerJoin('bibliography', 'authors.id', 'bibliography.author_id')
-  .innerJoin('books', 'bibliography.book_id', 'books.id')
-  .then(function(authors) {
-    fuckMyLife.killMeNow = authors;
-    console.log(fuckMyLife.killMeNow);
-    fuckMyLife.killMeNow.forEach(function(e){
+router.get('/', function(req, res, next){
+  var authorz =[]
 
+  return knex('authors')
+  .then(function(authors){
+    for (var i = 0; i < authors.length; i++) {
+      authorz.push({
+        id: authors[i].id,
+        last_name: authors[i].last_name,
+        first_name: authors[i].first_name,
+        url: authors[i].portrait_url,
+        bio: authors[i].biography,
+        titles: []});
+      }
+
+      return knex('authors')
+      .innerJoin('bibliography', 'authors.id', 'bibliography.author_id')
+      .innerJoin('books', 'bibliography.book_id', 'books.id')
+      .select('books.title', 'bibliography.author_id')
     })
-
-  res.render('authors', {authors: authors});
-  })
-
-});
-
-router.get('/:id', function(req, res, next){
-  return knex('authors').where({'authors.id': req.params.id}).first()
-  .then(function(author){
-    return knex('bibliography')
-    .where({'bibliography.author_id': req.params.id})
-    .pluck('book_id')
-    .then(function(booksId){
-      return knex('books')
-      .whereIn('books.id', booksId)
-      .then(function(books){
-      console.log(author);
-      console.log(books);
-      res.render('authordetail', {author: author, books: books})
-    })
+    .then(function(data){
+      for (var i = 0; i < authorz.length; i++) {
+        for (var j = 0; j < data.length; j++) {
+          if (authorz[i].id == data[j].author_id){
+            authorz[i].titles.push(data[j].title)
+          }
+        }
+      }
+      res.render('authors', {authors: authorz, books: authorz});
     })
   })
-})
+
+
+  router.get('/:id', function (req, res, next){
+    return knex('authors')
+    .where('authors.id', req.params.id)
+    .innerJoin('bibliography', 'authors.id', 'bibliography.author_id')
+    .innerJoin('books', 'bibliography.book_id', 'books.id')
+    .select('books.title', 'authors.first_name', 'authors.last_name', 'authors.biography', 'authors.portrait_url')
+    .then(function(data){
+      var books=[];
+      for (var i = 0; i < data.length; i++) {
+        books.push(data[i].title)
+      }
+      res.render('authordetail',{books: books, author: data[0]});
+    })
+  })
+
 
 module.exports = router;
