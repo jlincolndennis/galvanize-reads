58cc4e2f4f641fc9f68bf27ae763ecb81f781282
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2016-04-08 22:51:25 -0600

    preparing my ass for heroku

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,28 @@
-// Update with your config settings.
+require('dotenv').load();
+
 module.exports = {
+  debug: true,
 
   development: {
     client: 'postgresql',
-    connection: 'postgres://localhost/galvanize-reads'
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    }
   },
-  pool: {
-    min: 2,
-    max: 10
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    }
   },
+
   seeds: {
-    directory: './seeds/'
+  directory: './seeds/'
   }
 
 };
--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var knex = require('knex')(require('../knexfile')['development']);
+var knex = require('knex')(require('../knexfile')[process.env.DB_ENV]);
 
 router.get('/', function(req, res, next){
   var authorz =[]
